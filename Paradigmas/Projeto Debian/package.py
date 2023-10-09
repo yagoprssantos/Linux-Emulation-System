1be06b3a682c654435ef9a95f50e5b6c073790df
Yago Santos <yagprogramr@gmail.com>
2023-10-09 08:45:48 -0300

    Commit Inheritance Repository-Package

--- Paradigmas/Projeto Debian/package.py
+++ Paradigmas/Projeto Debian/package.py
@@ -1,32 +1,33 @@
-import time
+import time, repository
 from loading import LoadingAnimation
 
-class Package:
+class Package(repository.Repository):
     # Classe que representa um pacote de software instalável.
-    def __init__(self, name, version):
+    def __init__(self, address, reponame, packname, version):
+        super().__init__(address, reponame)
         # Inicializa um pacote de software com um nome e versão.
-        self.name = name
+        self.packname = packname
         self.version = version
         self.installed = False  # Estado de instalação
 
     def install(self):
         # Instala o pacote de software.
         if not self.installed:
-            print(f"Instalando {self.name}; Versão {self.version}", end='')
+            print(f"Instalando {self.packname}; Versão {self.version}", end='')
             LoadingAnimation(1)
             self.installed = True
-            print(f"Pacote '{self.name}' instalado.\n")
+            print(f"Pacote '{self.packname}' instalado.\n")
         else:
-            print(f"Pacote '{self.name}' já está instalado.\n")
+            print(f"Pacote '{self.packname}' já está instalado.\n")
 
     def uninstall(self):
         # Desinstala o pacote de software.
         if self.installed:
-            print(f"Desinstalando {self.name}; Versão {self.version}", end='')
+            print(f"Desinstalando {self.packname}; Versão {self.version}", end='')
             LoadingAnimation(1)
             time.sleep(2)
             self.installed = False
-            print(f"Pacote '{self.name}' desinstalado.")
+            print(f"Pacote '{self.packname}' desinstalado.")
         else:
-            print(f"Pacote '{self.name}' não está instalado.")
+            print(f"Pacote '{self.packname}' não está instalado.")
 
--- Paradigmas/Projeto Debian/repository.py
+++ Paradigmas/Projeto Debian/repository.py
@@ -4,15 +4,15 @@ from package import Package
 
 class Repository:
     # Classe que armazena e gerencia pacotes de software.
-    def __init__(self, address, name):
+    def __init__(self, address, reponame):
         # Inicializa o repositório com um endereço e nome específicos.
         self.address = address
-        self.name = name
+        self.reponame = reponame
         self.repo_pack_list = []
 
-    def DownloadRepo(self, name):
+    def DownloadRepo(self, reponame):
         # Baixa pacotes do repositório.
-        print(f"Endereço: {self.address}\nBaixando pacotes do repositório {self.name}", end='')
+        print(f"Endereço: {self.address}\nBaixando pacotes do repositório {self.reponame}", end='')
         LoadingAnimation(1)
         time.sleep(1)
 
@@ -20,34 +20,34 @@ class Repository:
         for package in self.repo_pack_list:
             package.install()
 
-        print(f"Pacotes do repositório {self.name} baixados.\n")
+        print(f"Pacotes do repositório {self.reponame} baixados.\n")
 
     def ListPackages(self):
         # Lista os pacotes disponíveis no repositório.
-        print(f"Pacotes no repositório {self.name}:")
+        print(f"Pacotes no repositório {self.reponame}:")
         for package in self.repo_pack_list:
-            print(f"- Nome: {package.name}")
+            print(f"- Nome: {package.reponame}")
             print(f"  Versão: {package.version}")
             print(f"  Instalado: {'Sim' if package.installed else 'Não'}\n")
 
     def InstallPackage(self, package_name):
         # Instala um pacote pelo nome, se existir no repositório.
-        package = next((p for p in self.repo_pack_list if p.name == package_name), None)
+        package = next((p for p in self.repo_pack_list if p.reponame == package_name), None)
         if package:
             package.install(package)
         else:
-            print(f"Pacote '{package_name}' não encontrado no repositório {self.name}.")
+            print(f"Pacote '{package_name}' não encontrado no repositório {self.reponame}.")
 
     def UninstallPackage(self, package_name):
         # Desinstala um pacote pelo nome, se existir e estiver instalado.
-        package = next((p for p in self.repo_pack_list if p.name == package_name), None)
+        package = next((p for p in self.repo_pack_list if p.reponame == package_name), None)
         if package:
             package.uninstall(package)
         else:
-            print(f"Pacote '{package_name}' não encontrado no repositório {self.name}.")
+            print(f"Pacote '{package_name}' não encontrado no repositório {self.reponame}.")
 
     def ListRepositories(self):
         for package in self.repo_pack_list:
-            print(f"- Nome: {package.name}")
+            print(f"- Nome: {package.reponame}")
             print(f"  Versão: {package.version}")
             print(f"  Instalado: {'Sim' if package.installed else 'Não'}")
